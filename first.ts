10e4f120c68fc63cd3d075478c88c35494085179
whereisjw <j9399977@gmail.com>
2023-10-05 12:00:00 +0900

    커스텀 타입 가드

--- first.ts
+++ first.ts
@@ -97,4 +97,62 @@ type ObjAB = A|B
   return undefined
 } */
 
+//원시값 타입가드
+/* function numOrStr(a: number | string) {
+  if (typeof a === 'string') {
+    a.split(',');  
+  } else {
+    a.toFixed(1);
+  }
+  
+}
 
+ function numOrNumArr(a: number | number[]) {
+  if (Array.isArray(a)) {
+    a.slice(1);  
+  } else {
+    a.toFixed(1);
+  }
+}
+
+
+type B = { type: 'b', bbb: string };
+type C = { type: 'c', ccc: string };
+type D = { type: 'd', ddd: string };
+type A = B | C | D;
+function typeCheck(a: A) {
+  if (a.type === 'b') {
+    a.bbb;
+  } else if (a.type === 'c') {
+    a.ccc;
+  } else {
+    a.ddd;
+  }
+}
+ */
+
+/*
+interface Cat { meow: number }
+interface Dog { bow: number }
+function catOrDog(a: Cat | Dog): a is Dog {
+  if ((a as Cat).meow) { return false }
+  return true;
+}
+const cat: Cat | Dog = { meow: 3 }
+if (catOrDog(cat)) {
+    console.log(cat.meow);
+}
+if ('meow' in cat) {
+    console.log(cat.meow);
+}
+
+const isRejected = (input: PromiseSettledResult<unknown>): input is PromiseRejectedResult => input.status === 'rejected';
+const isFulfilled = <T>(input: PromiseSettledResult<T>): input is PromiseFulfilledResult<T> => input.status === 'fulfilled';
+
+const promises = await Promise.allSettled([Promise.resolve('a'), Promise.resolve('b')]);
+const errors = promises.filter(isRejected);
+ */
+
+
+/* Promise -> Pending -> Settled(resovled, Rejected) */
+promsiese.them().catch()
