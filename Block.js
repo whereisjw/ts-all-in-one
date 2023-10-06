b62a0683cc51a2a1af6774e4a499eeee360b286c
whereisjw <j9399977@gmail.com>
2023-10-06 12:00:00 +0900

    how typescript work

---
+++ Block.js
@@ -0,0 +1,9 @@
+var nico = {
+    nickname: "nick"
+};
+/* nico.hello()   //이러면 에러남 */
+/* [1,2,3,4,5,6,7,8,9,10] + false 이래도 에러남 */
+function divide(a, b) {
+    return a / b;
+}
+/* divide('hi')  파라미터의 값의 형태 뿐만아니라 갯수도 체크를 해준다*/ 
