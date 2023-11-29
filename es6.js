80466a4d7d084831962e75852223668479b2eb25
whereisjw <j9399977@gmail.com>
2023-11-29 12:00:00 +0900

    es6 스코프체인

---
+++ es6.js
@@ -0,0 +1,17 @@
+/* function c() {
+    const y = "y";
+    console.log("c");
+    function b() {
+      const z = "z";
+      console.log("b");
+      c();
+    }
+  }
+  
+  function a() {
+    const x = "x";
+    console.log("a");
+    b();
+  }
+  
+  a(); //  a 찍히고 레퍼런스에러 b is not define */
---
+++ es6.md
@@ -0,0 +1,21 @@
+### 스코프체인
+
+```
+function c() {
+  const y = "y";
+  console.log("c");
+  function b() {
+    const z = "z";
+    console.log("b");
+    c();
+  }
+}
+
+function a() {
+  const x = "x";
+  console.log("a");
+  b();
+}
+
+a(); //  a 찍히고 레퍼런스에러 b is not define
+```
